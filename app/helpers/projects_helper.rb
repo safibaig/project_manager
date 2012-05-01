5f39e8d17db55f58db6e1e013c146686eb3ae09b
Abraham Kuri <abraham.kuri@gmail.com>
2012-04-30 22:09:41 -0500

    project creation fix

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,39 +1,47 @@
 module ProjectsHelper
   
-  def data_client(client)
+  def data_client
+    unless @project.client.nil?
        attributes = {}
        array = []
-       attributes[:id] = client.id
-       attributes[:company_name] = client.company_name
+       attributes[:id] = @project.client.id
+       attributes[:company_name] = @project.client.company_name
        array << attributes
        array.to_json
+     end
    end
    
-   def data_business_unit(business_unit)
+   def data_business_unit
+    unless @project.business_unit.nil?
       attributes = {}
       array = []
-      attributes[:id] = business_unit.id
-      attributes[:name] = business_unit.name
+      attributes[:id] = @project.business_unit.id
+      attributes[:name] = @project.business_unit.name
       array << attributes
       array.to_json
+    end
    end
    
-   def data_sales_rep(user)
+   def data_sales_rep
+    unless @project.user.nil? 
      attributes = {}
      array = []
-     attributes[:id] = user.id
-     attributes[:name] = user.name
+     attributes[:id] = @project.user.id
+     attributes[:name] = @project.user.name
      array << attributes
      array.to_json
+    end
    end
    
-   def data_pm(user)
+   def data_pm
+    unless @project.user.nil?
      attributes = {}
      array = []
-     attributes[:id] = user.id
-     attributes[:name] = user.name
+     attributes[:id] = @project.user.id
+     attributes[:name] = @project.user.name
      array << attributes
      array.to_json
+    end
    end
   
 end
